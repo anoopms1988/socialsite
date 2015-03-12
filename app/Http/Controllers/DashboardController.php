74b77ce2293ba2e882df4fcf1487d20765e83b4f
anoopms <anoop.ms@inapp.com>
2015-03-12 11:35:20 +0530

    Dashboard controller for dashboard handling

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+         $this->middleware('auth');
+    }
+    
+      /**
+     * Create a new controller instance.
+     *
+     * @return view
+     */
+     public function viewDashboard() {
+        return view('home.dashboard');
+    }
+}
